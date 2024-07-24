248344c3e4693fdbfac8c04f0e7fb771f0b2ec04
Jack12404 <jackli@computingforall.org>
2024-07-24 16:41:25 -0700

    eddited the syle of watchlist

---
+++ main.js
@@ -0,0 +1,25 @@
+//Movie containers with buttons; saves the movie to the users watchlist
+function saveToWatchedlist() {
+
+}
+
+//Nav Bar
+
+
+// Home Page; get #1 movie, get popular movies, get popular tv shows
+
+function loadWithLarge() {
+
+}
+
+// Genre page; get movies by genre and show them
+
+function loadWithRegular() {
+    
+}
+
+// Watchlist; get your list, most recently watched movies, recommendation based on watched movies
+
+function loadWithSmall() {
+    
+}
