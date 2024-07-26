d46853c3de527c002e33e7b3b93bfd9e76b387fa
Jack12404 <jackli@computingforall.org>
2024-07-26 16:18:33 -0700

    getting movie info with api & start of acessing movie info from storage.

--- watchlist.js
+++ watchlist.js
@@ -1,8 +1,34 @@
 const recent = document.getElementById('recent');
 const myWatchList = document.getElementById('myWatchList');
 const recommendation = document.getElementById('recommendation');
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzODViYTQzNmE1Y2QxZWRmMTk2M2E0NmVjZmJkNjc2YSIsIm5iZiI6MTcyMjAyMDcyNi42MzYxNzMsInN1YiI6IjY2YTNlYWM4YjQxYjY1NDY5ZWIxMjRmZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BHx470qugTDBzjuyZaAdTl1xlID7qk5lCSNzwhxV6YA'
+    }
+  };
 
-function addMovieToList(list, front) {
+async function getMoveById(id) {
+    try {
+        let reponseMovie = await fetch('https://api.themoviedb.org/3/movie/' + id + '?language=en-US', options);
+        return await reponseMovie.json();
+    }catch(error) {
+        console.error(error);
+    }
+}
+
+function getWatchlist() {
+    let keys = Object.keys(localStorage);
+    let total = Object.keys(localStorage).length;
+    if (total > 0) {
+        for(let key of keys) {
+            let movie = getMoveById(id);
+        }
+    }
+}
+
+function loadMovieToList(list, front, movieNameDate, movieGenreAgeRating, movieImage) {
     //get movie info
     let movieNameDate = "Fight Club (1999)";
     let movieGenreAgeRating = "Action. Rated R"
@@ -31,9 +57,12 @@ function addMovieToList(list, front) {
 }
 
 //testing
+
+let list = [1, 2, 3, 4];
+let movieobjects = list.map(getMoveById);
 let x = 0 
 while (x < 10) {
-    addMovieToList(myWatchList, false)
+    loadMovieToList(myWatchList, false)
     x++;
 }
 
